bbe6aabfbfe71b4cd424526c0e6c3c7cb58413ba
Daniel Souza <workdndsza@gmail.com>
2024-09-10 14:18:41 -0300

    estrutura do projeto

---
+++ src/test/java/com/dnsouzadev/social_network/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnsouzadev.social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
